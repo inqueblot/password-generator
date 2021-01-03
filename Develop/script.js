93cf5ac0d5fa5b4c5831c2a4a0bda0b212dd6876
inqueblot <notminebydesign@gmail.com>
2021-01-03 00:56:11 -0600

    generated password now returns to website

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,61 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const passwordLength = Number(prompt("How many character's is your password?", "8-128"));
+  const checkLower = confirm("Does this password contain a lowercase letter?");
+  const checkUpper = confirm("Does this password contain a uppercase letter?");
+  const checkNumber = confirm("Does this password contain a number?");
+  const checkSpecial = confirm("Does this password contain a special character?");
+
+  //console.log(typeof passwordLength);
+
+  let random = '';
+
+  for (let index = 0; index < passwordLength; index++) {
+    while (random.length < passwordLength) {
+
+
+      if (checkLower == true) {
+        random += lowerCaseChars()
+      };
+
+      if (checkUpper == true) {
+        random += upperCaseChars()
+      };
+
+      if (checkNumber == true) {
+        random += numberChars()
+      };
+
+      if (checkSpecial == true) {
+        random += symbolChars()
+      };
+
+
+    }
+
+    var lessRandom = random.slice(0, passwordLength)
+
+    function shuffle() {
+      var arr = lessRandom.split('');           // Convert String to array
+
+      arr.sort(function () {
+        return 0.5 - Math.random();
+      });
+      moreRandom = arr.join('');                // Convert Array to string
+      return moreRandom;                        // Return shuffled string
+    }
+  }
+
+  shuffle()
+  var password = moreRandom;
+  var passwordText = document.querySelector("moreRandom");
 
   passwordText.value = password;
+
+  console.log(random)
+  console.log(lessRandom)
+  console.log(moreRandom)
 
 }
 
@@ -14,58 +65,13 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //code to create alert prompts and create password
-const passwordLength = Number(prompt("How many character's is your password?", "8-128"));
-const checkLower = confirm("Does this password contain a lowercase letter?");
-const checkUpper = confirm("Does this password contain a uppercase letter?");
-const checkNumber = confirm("Does this password contain a number?");
-const checkSpecial = confirm("Does this password contain a special character?");
-
-//console.log(typeof passwordLength);
-
-let random = '';
-
-for (let index = 0; index < passwordLength; index++) {
-  while (random.length < passwordLength) {
 
 
-    if (checkLower == true) {
-      random += lowerCaseChars()
-    };
-
-    if (checkUpper == true) {
-      random += upperCaseChars()
-    };
-
-    if (checkNumber == true) {
-      random += numberChars()
-    };
-
-    if (checkSpecial == true) {
-      random += symbolChars()
-    };
 
 
-  }
-
-  var lessRandom = random.slice(0, passwordLength)
-
-  function shuffle() {
-    var arr = lessRandom.split('');           // Convert String to array
-
-    arr.sort(function () {
-      return 0.5 - Math.random();
-    });
-    moreRandom = arr.join('');                // Convert Array to string
-    return moreRandom;                        // Return shuffled string
-  }
-}
-
-shuffle()
 
 
-console.log(random)
-console.log(lessRandom)
-console.log(moreRandom)
+
 
 function lowerCaseChars() {
   return String.fromCharCode(97 + Math.floor(Math.random() * 26));
@@ -75,7 +81,7 @@ function upperCaseChars() {
   return String.fromCharCode(65 + Math.floor(Math.random() * 26));
 }
 function symbolChars() {
-  var symbols = "!@#$%^&*()[]{}/,.?"; //check symbols to OWASP
+  var symbols = " !#$%&'()*+,-./:;<=>?@[]\^_`{|]~"; //check symbols to OWASP
   return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
